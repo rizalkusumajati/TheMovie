19f66d36970b54bc6dbc2af571a9f44d68c559bb
Rizal Kusumajati Nugroho <rizalkusumajatinugroho@gmail.com>
2020-10-15 11:38:21 +0700

    Initial commit

---
+++ app/src/main/java/com/riztech/themovie/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.riztech.themovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
